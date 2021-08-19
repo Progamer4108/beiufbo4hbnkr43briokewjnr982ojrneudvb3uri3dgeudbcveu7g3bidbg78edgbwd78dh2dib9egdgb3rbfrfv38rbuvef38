14e127bfd28436126799521e1d16f16be4bb7a7f
Progamer4108 <77099411+Progamer4108@users.noreply.github.com>
2021-08-19 17:49:59 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -33,8 +33,8 @@ function send() {
     console.log(charAt3);
 
     remove_charAt1 = word.replace(charAt1, "_");
-    remove_charAt2 = word.replace(charAt2, "_");
-    remove_charAt3 = word.replace(charAt3, "_");
+    remove_charAt2 = remove_charAt1.replace(charAt2, "_");
+    remove_charAt3 = remove_charAt2.replace(charAt3, "_");
     console.log(remove_charAt3);
 
     question_word = "<h4 id='word_display'> Q. " + remove_charAt3 + "</h4>";
